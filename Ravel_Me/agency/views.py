0917d62f5071373d16efcc48288184d649aee4d1
Iva Vukicevic <vuk.iva@gmail.com>
2014-08-24 22:17:10 -0400

    Fixed bug about non-existant users

--- Ravel_Me/agency/views.py
+++ Ravel_Me/agency/views.py
@@ -5,9 +5,11 @@ import numpy as np
 from saving import load_sparse_csr, unpickle
 import redis
 from json import loads, dumps
+from secret import pw
 
-db = mdb.connect(user="root", host="localhost", db="Ravelry", charset='utf8')
+db = mdb.connect("localhost", "iva", pw, db="Ravelry", charset='utf8')
 q = redis.Redis(unix_socket_path='/var/run/redis/redis.sock')
+username_list = set(unpickle('username_list'))
 
 @agency.route('/')
 @agency.route('/index')
@@ -17,20 +19,24 @@ def index():
 @agency.route("/projects")
 def projects_json():
 	user = request.args.get('username')
-	craft = request.args.get('craft')
-	category = request.args.get('category')
-	q.rpush("tasks", dumps([user, craft, category]))
-	recs = loads(q.blpop("results")[1].decode('utf-8'))
-	print(recs)
-	with db:
-		cur = db.cursor()
-		# FIX IT: The order of recommendations is not preserved:
-		cur.execute("SELECT pattern_name, pattern_id, pattern_photo FROM Photos WHERE pattern_id IN (%s,%s,%s,%s,%s);", tuple(recs))
-		base_url = 'http://www.ravelry.com/patterns/library/'
-		projects = []
-		for name in cur:
-			projects.append(dict(name="<a href={0}{1}>{2}</a>".format(base_url, name[1], name[0].encode('utf-8')), pic="<img src={0}>".format(name[2])))
-		return jsonify(dict(projects=projects))
+	if user in username_list:
+		craft = request.args.get('craft')
+		category = request.args.get('category')
+		q.rpush("tasks", dumps([user, craft, category]))
+		recs = loads(q.blpop("results")[1].decode('utf-8'))
+		with db:
+			cur = db.cursor()
+			# FIX IT: The order of recommendations is not preserved:
+			cur.execute("SELECT pattern_name, pattern_id, pattern_photo FROM Photos WHERE pattern_id IN (%s,%s,%s,%s,%s);", tuple(recs))
+			base_url = 'http://www.ravelry.com/patterns/library/'
+			projects = []
+			for name in cur:
+				projects.append(dict(name="<a href={0}{1}>{2}</a>".format(base_url, name[1], name[0].encode('utf-8')), pic="<img src={0}>".format(name[2])))
+			return jsonify(projects=projects)
+	else:
+		error = []
+		error.append({'pic' : 'Invalid Username', 'name': ''})
+		return jsonify(projects=error)
  
 @agency.route("/jquery")
 def index_jquery():
